cc9141f02f06dd8c83b50a34415d6d7bd7295715
葛剑 <gejian02@innotechx.com>
2020-12-14 17:20:00 +0800

    isValid

--- code/LeetCode/TowSum.js
+++ code/LeetCode/TowSum.js
@@ -3,16 +3,21 @@
  * @param {*} nums 
  * @param {*} target 
  * @return {number[]}
+ * 
+ * 解题思路：通过HashMap来实现
+ * 1、创建一个map来装载数据，
+ * 2、通过key与val来查询map中是否包含taget - nums[index]的值
+ * 3、如果包含，那么就输出结果，不包含就把数据放入map中
  */
 var twoSum = function( nums, target ){
   let map = {};
   for(let i = 0; i < nums.length; i++){
-    let preIndex = target - nums[i]
-    if( map.hasOwnProperty(preIndex)){
-      return [map[preIndex],i]
+    let preIndex = target - nums[i];
+    if( map.hasOwnProperty(preIndex) ){
+      return [map[preIndex], i];
     }else{
-      map[ nums[i] ] = i
+      map[ nums[i] ] = i;
     }
   }
 }
-console.log(twoSum([2,5,3,1,6,7], 9)) 
+console.log(twoSum([2, 5, 3, 1, 6, 7], 9));
