de58c9b90a8099f971757ea5fb6c827290e71781
gj_kester <415950234@qq.com>
2020-12-26 18:06:15 +0800

    leetcode maxSubArray

---
+++ code/LeetCode/maxSubArray.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @param {*} nums 
+ */
+var maxSubArray = function (nums){
+  const memo = [];
+  memo[0] = nums[0];
+
+  for( let i = 1; i < nums.length; i++){
+    memo[i] = Math.max(num[i] + memo[i-1], nums[i]);
+  }
+
+  let max = nums[0];
+
+  for(let i = 1; i < memo.length; i++){
+    max = Math.max(max, memo[i]);
+  }
+
+  return max;
+}
