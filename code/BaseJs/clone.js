2a05ef19b30c3e70cf1f99562058a61c653cf60e
gj_kester <415950234@qq.com>
2021-01-19 10:58:03 +0800

    update: 	deepClonePro  deepClone 升级版本

--- code/BaseJs/clone.js
+++ code/BaseJs/clone.js
@@ -35,6 +35,31 @@ function deepClone( origin, target ){
   return target
 }
 
+function deepClonePro(target,map = new WeakMap()){
+  if(typeof target ==='object' && target !== null){
+      if (target instanceof Date) return new Date(target);        
+      if (target instanceof RegExp) return new RegExp(target);
+      // 利用weakMap处理循环引用，有就直接返回
+      if (hash.has(target)) return hash.get(target);
+
+      //获得所有属性的描述信息，可处理带有比如enumerable、set、get等描述信息的数据
+      let allDesc = Object.getOwnPropertyDescriptors(target);
+      //创建一个新对象，使用现有的对象来提供新创建的对象的proto
+      let result = Object.create(Object.getPrototypeOf(target), allDesc);
+      //以上两行逻辑实现了浅拷贝并且处理了原型链
+
+      hash.set(target, result);
+      //Reflect.ownKeys 可获得不可遍历属性和Symbol属性
+      for(let key of Reflect.ownKeys(target)){          
+          result[key] = deepClone(target[key],hash)       
+      }        
+      return result;
+  }else{
+     //基础类型、function。
+     return target;    
+  }
+}
+
 
 var cloneObj = clone(OrgObj)
 var deepCloneObj = deepClone(OrgObj)
