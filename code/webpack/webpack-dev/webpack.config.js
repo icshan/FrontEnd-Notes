cf64d909d4a1c1e5f72f7237f01fe80509ce1619
gj_kester <415950234@qq.com>
2021-07-06 15:44:23 +0800

    mini-webpack  添加对loader以及plugin处理
    手写简易 less-loader 以及 style-loader

--- code/webpack/webpack-dev/webpack.config.js
+++ code/webpack/webpack-dev/webpack.config.js
@@ -1,5 +1,11 @@
 let path = require('path');
-
+class P{
+  apply(compiler){
+    compiler.hooks.emit.tap('emit', function(){
+      console.log('emit~')
+    })
+  }
+}
 
 module.exports = {
   mode: 'development',
@@ -7,6 +13,17 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
-
+  },
+  module: {
+    rules:[{
+      test: /\.less$/,
+      use: [
+        path.resolve(__dirname, 'loader', 'style-loader'),
+        path.resolve(__dirname, 'loader', 'less-loader')
+      ]
+    }]
+  },
+  plugins:[
+    new P()
+  ]
 }
