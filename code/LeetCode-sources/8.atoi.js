cc61298c12756b7dbf543b333513312012b37dfd
gj_kester <415950234@qq.com>
2021-07-29 00:38:04 +0800

    leetcode

--- code/LeetCode-sources/8.atoi.js
+++ code/LeetCode-sources/8.atoi.js
@@ -9,6 +9,30 @@
  * - 推入数字
  *   result = result * 10 + num
  */
+/**
+ * @param {string} s
+ * @return {number}
+ */
  var myAtoi = function(s) {
-
+  if(s == null) return 0;
+  s = s.trim();
+  if(s == '') return 0;
+  let idx = 0,
+      sign = '+';
+  if(s[idx] == '+'){
+    idx++;
+  }else if(s[idx] == '-'){
+    sign = '-';
+    idx++;
+  }else if(s[idx].charCodeAt()<48 || s[idx].charCodeAt() > 57){
+    return 0
+  }
+  let str=""
+  while(s[idx] && s[idx].charCodeAt() >= 48 && s[idx].charCodeAt() <= 57){
+    str+=s[idx++];
+  }
+  let res = Number(sign+str) || 0;
+  res = Math.max((-2)**31, res);
+  res = Math.min(2**31-1, res);
+  return res;
 };
